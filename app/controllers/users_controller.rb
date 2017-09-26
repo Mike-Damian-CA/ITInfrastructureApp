b9d50193a1c4cb72061103f3e8a024e4583ff4e9
Mike Damian CA <vlad.damian92@gmail.com>
2017-09-26 17:55:53 -0400

    added a user database and table similar to devices.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @user = User.all
   end
 
   # GET /users/1
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.fetch(:user).permit(:name, :dob)
     end
 end
