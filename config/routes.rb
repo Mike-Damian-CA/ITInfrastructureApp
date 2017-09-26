1f63dc1e7fb01fd29a150f4c7649754a411037d6
Mike Damian CA <vlad.damian92@gmail.com>
2017-09-25 22:11:40 -0400

    Made scaffold Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :devices
   root 'devices#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926012126) do
+ActiveRecord::Schema.define(version: 20170926020529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170926012126) do
     t.string "location", null: false
     t.string "device_type", null: false
     t.string "serial_number"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.date "dob"
   end
 
 end
